ee4f851838bd3715641115cf88ad76ef8ecc549c
poigrammer <atanas123atanasov@gmail.com>
2020-01-18 17:46:24 +0200

    update 1

--- HornerDiagram/Program.cs
+++ HornerDiagram/Program.cs
@@ -21,6 +21,8 @@ namespace HornerDiagram
 
         public static void Add()
         {
+            Console.WriteLine("Type only the coefficient and press Enter for each.\n" +
+                "For example (x^4 + 2x^3 - 3x - 45) ==> (1, 2, 0, -3, -45)");
             string input = Console.ReadLine();
             while (!string.IsNullOrEmpty(input))
             {
@@ -31,9 +33,9 @@ namespace HornerDiagram
         }
         public static void Calculate()
         {
-            for (int i = 1; i < Math.Abs(lastNumber); i++)
+            for (int i = lastNumber > 0 ? -lastNumber : lastNumber; i < Math.Abs(lastNumber); i++)
             {
-                if (lastNumber % i == 0)
+                if (i != 0 && lastNumber % i == 0)
                 {
                     List<int> tempList = new List<int>();
                     int temp = coefficient[0];
@@ -53,21 +55,26 @@ namespace HornerDiagram
         }
         public static void Print()
         {
+            if (dividers.Count == 0)
+            {
+                Console.WriteLine("No possible polynomials or invalid input.");
+                return;
+            }
             for (int i = 0; i < dividers.Count; i++)
             {
                 string result = "";
                 if (dividers[i] > 0) result = $"(x - {dividers[i]})(";
-                else result = $"(x + {dividers[i]})(";
+                else result = $"(x + {Math.Abs(dividers[i])})(";
                 for (int k = results[i].Count; k > 1; k--)
                 {
                     int tempNum = results[i][results[i].Count - k];
                     if (tempNum > 0 && k == results[i].Count)
                     {
-                        result += $"{tempNum}{(k-2 > 1 ? $"x^{k-2}" : "x")} ";
+                        result += $"{(tempNum > 1 ? tempNum.ToString() : "")}{(k-2 > 1 ? $"x^{k-2}" : "x")} ";
                         continue;
                     }
-                    else if (tempNum > 0) result += $"+ {tempNum}{(k - 2 > 1 ? $"x^{k - 2}" : "x")} ";
-                    else result += $"- {-tempNum}{(k - 2 > 1 ? $"x^{k - 2}" : "x")} ";
+                    else if (tempNum > 0) result += $"+ {(tempNum > 1 ? tempNum.ToString() : "")}{(k - 2 > 1 ? $"x^{k - 2}" : "x")} ";
+                    else if (tempNum < 0) result += $"- {-tempNum}{(k - 2 > 1 ? $"x^{k - 2}" : "x")} ";
                 }
                 result = result.Substring(0, result.Length - 2);
                 result += ")";
